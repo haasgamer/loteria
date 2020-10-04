32f32246877432111baf9534decc03b53f79967a
haasgamer <edson.haas@estudante.ifms.edu.brgit config --global user.name edson-haas>
2020-10-04 11:33:45 -0400

    Inclusão do sweetAlert2 no codigo

--- script.js
+++ script.js
@@ -1,61 +1,94 @@
-var numSort =[];
-var numEsco=[];
-
-function sortearNumeros() {
-  numSort=[];
-  let sort;
-  for (var i = 0; i < 6; i++) {
-    do {
-      let sort = Math.ceil(Math.random()*60);
-      sort=(sort==0)? 1 : sort;
-
-    } while (numSort.includes(sort)));
-      numSort.push(sort);
-  }
-
-}
-function addToList(num,pos) {
-  if (num.length==2) {
-    if (numEsco.includes(num)) {
-      alert("Numero Escolhido Anteriormente. Digite outro numero")
-
-    }else if (parseInt(num)>60) {
-        alert("O numero digitado nao pode ser ,aior que 60")
-    }else {
-      numEsco[pos-1]= num;
-    }
-  }
-
-}
-
-
-function verificarAcerto() {
-  sortearNumeros();
-  let cont =0;
-  if (numEsco.length != 6) {
-    alert("A quantidade de numeros escolhidos e menor que 6.\n Digite 6 numeros de 01 a 06 com duas casas decimal");
-
-  }else {
+var numSort = []
+var numEsco = []
+let contador;
+let rem;
+function Sorteio(){
+	let num= Math.floor(Math.random() * 60);
+	let li = document.createElement("li");
     for (var i = 0; i < 6; i++) {
-      if (numSort.includes(parseInt(numEsco[i]))) {
-        cont++;
-
-      }
-
+		numSort[i] =num;
+		if(numSort[i]<=9){
+			numSort[i]='0'+numSort[i];
+		}else{
+			numSort[i]=''+numSort[i];
+		}
+		num= Math.floor(Math.random() * 60);
+		while(numSort.indexOf(num)!=-1){
+			num= Math.floor(Math.random() * 60);
+		}
     }
-    printNumSort();
-    document.getElementById('totalAcertos').innerHTML="O total de acertos foi "+ cont++;
-  }
+
+	for (var i = 0; i < 6; i++){
+		li = document.createElement("li");
+		li.innerHTML = numSort[i];
+		document.getElementById("numSort").append(li);
+	}
+}
+function addToList(num, pos){
+    if(num.length == 2){
+        if(numEsco.indexOf(num)!=-1){
+            alert2("Erro","Número escolhido anteriormente. Digite outro número")
+        }else if(parseInt(num)>60){
+            alert2("Erro", "O números digitado não pode ser maior que 60")
+        }else{
+            numEsco[pos] = num;
+        }
+    }
+}
+
+function verificarAcertos(){
+
+
+	var node = document.getElementsByTagName("LI")[0];
+	for(var i =0;i<6;i++){
+		node = document.getElementsByTagName("LI")[0];
+		if (node.parentNode) {
+		  node.parentNode.removeChild(node);
+		}
+	}
+    let cont = 0;
+    if(numEsco.length != 6){
+        alert2("Erro", "A quantidade de números escolhidos é menor que 6.\n Digite 6 números de 01 a 60 com duas casas decimais")
+    }else{
+        for(var j=0; j<6; j++){
+            if(numSort.indexOf(numEsco[j]) != -1){
+                cont=cont+1;
+            }
+        }
+		Sorteio();
+        document.getElementById('totalAcertos').innerHTML = "O total de acertos foi " + cont;
+    }
 
 }
-function printNumSort() {
-  document.getElementById('numSort').innerHTML="";
-  for (var i = 0; i < numSort.length; i++) {
-    let li = document.createElement("li");
-    li.append(numSort[i]);
-    li.classList.add("result");
-    document.getElementById('numSort').append(li);
 
-  }
 
+
+function alert2(title, text){
+	let timerInterval
+	Swal.fire({
+	  title: title,
+	  html: text,
+	  timer: 5000,
+	  timerProgressBar: true,
+	  willOpen: () => {
+	    Swal.showLoading()
+	    timerInterval = setInterval(() => {
+	      const content = Swal.getContent()
+	      if (content) {
+	        const b = content.querySelector('b')
+	        if (b) {
+	          b.textContent = Swal.getTimerLeft()
+	        }
+	      }
+	    }, 1000)
+	  },
+	  onClose: () => {
+	    clearInterval(timerInterval)
+	  }
+	}).then((result) => {
+	  /* Read more about handling dismissals below */
+	  if (result.dismiss === Swal.DismissReason.timer) {
+	    console.log('O alerta foi fechado')
+	  }
+	})
 }
